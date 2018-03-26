a5f4fd72e01e901fdbcd355062b59569620f613d
Hiep Le <hieple@Hieps-MacBook-Pro.local>
2018-03-26 15:24:53 +0900

    build a simple model

--- new_train.py
+++ new_train.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
-import vis_lstm_model
-import data_loader
+import simple_model
+import data_loader1
 import argparse
 import numpy as np
 
@@ -11,7 +11,7 @@ def main():
                         help='num_lstm_layers')
     parser.add_argument('--fc7_feature_length', type=int, default=4096,
                         help='fc7_feature_length')
-    parser.add_argument('--rnn_size', type=int, default=512,
+    parser.add_argument('--rnn_size', type=int, default=128,
                         help='rnn_size')
     parser.add_argument('--embedding_size', type=int, default=512,
                         help='embedding_size'),
@@ -21,11 +21,11 @@ def main():
                         help='image_dropout')
     parser.add_argument('--data_dir', type=str, default='data',
                         help='Data directory')
-    parser.add_argument('--batch_size', type=int, default=200,
+    parser.add_argument('--batch_size', type=int, default=10,
                         help='Batch Size')
     parser.add_argument('--learning_rate', type=float, default=0.001,
                         help='Batch Size')
-    parser.add_argument('--epochs', type=int, default=200,
+    parser.add_argument('--epochs', type=int, default=20,
                         help='Expochs')
     parser.add_argument('--debug', type=bool, default=False,
                         help='Debug')
@@ -36,18 +36,20 @@ def main():
 
     args = parser.parse_args()
     print("Reading QA DATA")
-    qa_data = data_loader.load_questions_answers(args.version, args.data_dir)
+    qa_data = data_loader1.load_data(args.data_dir)
 
     print("Reading fc7 features")
-    fc7_features, image_id_list = data_loader.load_fc7_features(args.data_dir, 'train')
+    fc7_features, image_id_list = data_loader1.load_fc7_features(args.data_dir, 'train')
     print("FC7 features", fc7_features.shape)
     print("image_id_list", image_id_list.shape)
 
+    # map and image to an id
     image_id_map = {}
-    for i in xrange(len(image_id_list)):
+    for i in range(len(image_id_list)):
         image_id_map[image_id_list[i]] = i
 
-    ans_map = {qa_data['answer_vocab'][ans]: ans for ans in qa_data['answer_vocab']}
+    # id to word
+    ans_map = {qa_data['topic_vocab'][ans]: ans for ans in qa_data['topic_vocab']}
 
     model_options = {
         'num_lstm_layers': args.num_lstm_layers,
@@ -56,12 +58,12 @@ def main():
         'word_emb_dropout': args.word_emb_dropout,
         'image_dropout': args.image_dropout,
         'fc7_feature_length': args.fc7_feature_length,
-        'lstm_steps': qa_data['max_question_length'] + 1,
-        'q_vocab_size': len(qa_data['question_vocab']),
+        'lstm_steps': qa_data['max_conversation_length'] + 1,
+        'q_vocab_size': len(qa_data['conversation_vocab']),
         'ans_vocab_size': len(qa_data['answer_vocab'])
     }
 
-    model = vis_lstm_model.Vis_lstm_model(model_options)
+    model = simple_model.cnn_lstm_model(model_options)
     input_tensors, t_loss, t_accuracy, t_p = model.build_model()
     train_op = tf.train.AdamOptimizer(args.learning_rate).minimize(t_loss)
     sess = tf.InteractiveSession()
@@ -71,7 +73,7 @@ def main():
     if args.resume_model:
         saver.restore(sess, args.resume_model)
 
-    for i in xrange(args.epochs):
+    for i in range(args.epochs):
         batch_no = 0
 
         while (batch_no * args.batch_size) < len(qa_data['training']):
@@ -96,7 +98,7 @@ def main():
                 print("Loss", loss_value, batch_no, i)
                 print("Training Accuracy", accuracy)
 
-        save_path = saver.save(sess, "Data/Models/model{}.ckpt".format(i))
+        save_path = saver.save(sess, "data/models/model{}.ckpt".format(i))
 
 
 def get_training_batch(batch_no, batch_size, fc7_features, image_id_map, qa_data, split):
@@ -109,8 +111,8 @@ def get_training_batch(batch_no, batch_size, fc7_features, image_id_map, qa_data
     si = (batch_no * batch_size) % len(qa)
     ei = min(len(qa), si + batch_size)
     n = ei - si
-    sentence = np.ndarray((n, qa_data['max_question_length']), dtype='int32')
-    answer = np.zeros((n, len(qa_data['answer_vocab'])))
+    sentence = np.ndarray((n, qa_data['max_conversation_length']), dtype='int32')
+    answer = np.zeros((n, len(qa_data['topic_vocab'])))
     fc7 = np.ndarray((n, 4096))
 
     count = 0
